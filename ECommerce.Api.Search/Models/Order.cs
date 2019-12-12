2a143540535ac4f325070cef371d91fd65c7711e
Rodrigo Diaz Concha <rodrigo@rdiazconcha.com>
2019-12-12 11:21:47 -0800

    Removed the CustomerId property

--- ECommerce.Api.Search/Models/Order.cs
+++ ECommerce.Api.Search/Models/Order.cs
@@ -6,7 +6,6 @@ namespace ECommerce.Api.Search.Models
     public class Order
     {
         public int Id { get; set; }
-        public int CustomerId { get; set; }
         public DateTime OrderDate { get; set; }
         public decimal Total { get; set; }
         public List<OrderItem> Items { get; set; }
